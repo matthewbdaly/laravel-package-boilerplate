6e1c660a99181b606f1904544f7362d60644c767
Matthew Daly <matthewbdaly@gmail.com>
2017-12-31 20:22:43 +0000

    Amended base test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,9 +3,12 @@
 namespace Tests;
 
 use Orchestra\Testbench\TestCase as BaseTestCase;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
 
 class TestCase extends BaseTestCase
 {
+    use MockeryPHPUnitIntegration;
+
 	protected function getPackageProviders($app)
 	{
 		return ['Matthewbdaly\LaravelPackageBoilerplate\ServiceProvider'];
