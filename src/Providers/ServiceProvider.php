4a666b8c949c3590cccd23f3dcc0199a0275c16c
Matthew Daly <matthewbdaly@gmail.com>
2018-01-07 18:02:05 +0000

    Corrected routes path

--- src/Providers/ServiceProvider.php
+++ src/Providers/ServiceProvider.php
@@ -17,7 +17,7 @@ class ServiceProvider extends BaseServiceProvider
     public function boot()
     {
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        $this->loadRoutesFrom(__DIR__.'/../routes.php');
+        $this->loadRoutesFrom(__DIR__.'/../../routes.php');
         $this->loadViewsFrom(__DIR__.'/../views', 'packagename');
         $this->publishes([
             __DIR__.'/../views', resource_path('views/vendor/packagename')
